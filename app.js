7148c5c0edd4e35c37f6ff275b5ef6b1a0fb021e
NaveenDK <naveenkolambage@gmail.com>
2021-02-13 00:57:27 +1300

    added create post

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config()
 
 
 //db
 mongoose.connect(process.env.MONGO_URI,
-    {useNewUrlParser:true}
+    { useNewUrlParser:true }
     )
 .then(()=>
     console.log("DB Connected"))
@@ -26,6 +27,7 @@ const postRoutes = require('./routes/post');
 
 //middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json())
  
 
 
