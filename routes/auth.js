5b65e6ddab34eae442994a5f079948c47ba33b0a
NaveenDK <naveenkolambage@gmail.com>
2021-02-13 13:00:14 +1300

    validation for both orgs and indis

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,12 @@ const express = require('express');
 const {signup} = require("../controllers/auth");
 const {orgsignup} = require("../controllers/auth");
  
- 
+ const {individualSignupValidator,organizationSignupValidator} = require("../validator")
 
 const router = express.Router()
 
 
-router.post('/signup',  signup )
-router.post('/orgsignup',  orgsignup )
+router.post('/signup', individualSignupValidator,  signup )
+router.post('/orgsignup', organizationSignupValidator, orgsignup )
 
 module.exports = router;
--- validator/index.js
+++ validator/index.js
@@ -26,3 +26,95 @@ exports.createPostValidator = (req,res,next)=>{
     next();
 
 }
+
+exports.individualSignupValidator = (req,res,next) =>{
+
+    //name is not null and between 4 to 10 characters
+
+    req.check("name", "Name is required").notEmpty();
+
+    //email not null,valid and normalized
+    req.check("email", "Email must be between 3 to 32 characters")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
+    .isLength({
+        min:4,
+        max:2000
+    })
+
+    //organization should not be null
+    req.check("organization", "Organization is required").notEmpty();
+   
+    //mobile should not be empty
+    req.check("mobile", "Mobile is required").notEmpty();
+
+    req.check('mobile')
+    .isLength({min:8})
+    .withMessage("Mobile number should contain atleast 8 digits")
+
+    //check for password
+   req.check("password", "Password is required").notEmpty();
+   req.check('password')
+   .isLength({min:6})
+   .withMessage("Password must contain atleast 6 characters")
+   .matches(/\d/).withMessage("Password must contain a number")
+
+    //check for errors
+    //check for errors
+    const errors = req.validationErrors();
+    //pick the first out of the errors
+
+    if(errors){
+        const firstError =  errors.map((error)=>error.msg)[0]
+        return res.status(400).json({error: firstError})
+    }
+
+    next();
+
+    
+
+}
+exports.organizationSignupValidator = (req,res,next) =>{
+
+        //name is not null and between 4 to 10 characters
+
+    req.check("name", "Name is required").notEmpty();
+
+    //email not null,valid and normalized
+    req.check("email", "Email must be between 3 to 32 characters")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
+    .isLength({
+        min:4,
+        max:2000
+    })
+
+     
+   
+    //mobile should not be empty
+    req.check("mobile", "Mobile is required").notEmpty();
+
+    req.check('mobile')
+    .isLength({min:8})
+    .withMessage("Mobile number should contain atleast 8 digits")
+    
+    //check for password
+   req.check("password", "Password is required").notEmpty();
+   req.check('password')
+   .isLength({min:6})
+   .withMessage("Password must contain atleast 6 characters")
+   .matches(/\d/).withMessage("Password must contain a number")
+
+    //check for errors
+    //check for errors
+    const errors = req.validationErrors();
+    //pick the first out of the errors
+
+    if(errors){
+        const firstError =  errors.map((error)=>error.msg)[0]
+        return res.status(400).json({error: firstError})
+    }
+
+    next();
+
+}
